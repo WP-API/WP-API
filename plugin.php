3c93b70330e675bb2e9a52f886830a9a13149004
Ryan McCue <me@ryanmccue.info>
2013-09-28 12:15:46 +1000

    Bump JS version to 0.6

--- plugin.php
+++ plugin.php
@@ -118,7 +118,7 @@ register_deactivation_hook( __FILE__, 'json_api_activation' );
  * Register our API Javascript helpers
  */
 function json_register_scripts() {
-	wp_register_script( 'wp-api', plugins_url( '/wp-api.js', __FILE__ ), array( 'jquery', 'backbone', 'underscore' ), '0.5', true );
+	wp_register_script( 'wp-api', plugins_url( '/wp-api.js', __FILE__ ), array( 'jquery', 'backbone', 'underscore' ), '0.6', true );
 	wp_localize_script( 'wp-api', 'wpApiOptions', array( 'base' => json_url() ) );
 }
 add_action( 'wp_enqueue_scripts', 'json_register_scripts', -100 );
