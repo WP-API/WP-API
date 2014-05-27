49e56d6bfb6880726e79eb253471692051ccf3ae
Ryan McCue <me@ryanmccue.info>
2014-05-27 21:39:23 +1000

    Check that parent ID is valid

--- lib/class-wp-json-posts.php
+++ lib/class-wp-json-posts.php
@@ -1259,6 +1259,10 @@ class WP_JSON_Posts {
 		// Parent
 		if ( ! empty( $data['parent'] ) ) {
 			$parent = get_post( $data['parent'] );
+			if ( empty( $parent ) ) {
+				return new WP_Error( 'json_post_invalid_id', __( 'Invalid post parent ID.' ), array( 'status' => 400 ) );
+			}
+
 			$post['post_parent'] = $parent->ID;
 		}
 
