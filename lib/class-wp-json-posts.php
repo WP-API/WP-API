10c7750826e3b04ae3cf7cdc0bc0d75afd5ac989
Daniel Bachhuber <daniel@handbuilt.co>
2014-06-01 13:54:49 -0700

    get_post() safely handles $id, remove dupe error check

--- lib/class-wp-json-posts.php
+++ lib/class-wp-json-posts.php
@@ -311,13 +311,9 @@ class WP_JSON_Posts {
 	public function get_post( $id, $context = 'view' ) {
 		$id = (int) $id;
 
-		if ( empty( $id ) ) {
-			return new WP_Error( 'json_post_invalid_id', __( 'Invalid post ID.' ), array( 'status' => 404 ) );
-		}
-
 		$post = get_post( $id, ARRAY_A );
 
-		if ( empty( $post['ID'] ) ) {
+		if ( empty( $id ) || empty( $post['ID'] ) ) {
 			return new WP_Error( 'json_post_invalid_id', __( 'Invalid post ID.' ), array( 'status' => 404 ) );
 		}
 
