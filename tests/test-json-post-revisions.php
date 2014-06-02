a9f1dac205894625ed9ce7887ba717f4f3bd2750
Daniel Bachhuber <d@danielbachhuber.com>
2014-06-01 19:55:14 -0500

    Test access to revisions

---
+++ tests/test-json-post-revisions.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Unit tests covering WP_JSON_Posts revisions functionality.
+ *
+ * @package WordPress
+ * @subpackage JSON API
+ */
+
+class WP_Test_JSON_Post_Revisions extends WP_UnitTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->author = $this->factory->user->create( array( 'user_role' => 'author' ) );
+		$this->contributor = $this->factory->user->create( array( 'user_role' => 'contributor' ) );
+		$this->post_id = $this->factory->post->create( array(
+			'post_author' => $this->author,
+			'post_title' => md5( wp_generate_password() ),
+			'post_content' => md5( wp_generate_password() )
+			) );
+		$this->revision_id = wp_save_post_revision( $this->post_id );
+
+		$this->fake_server = $this->getMock('WP_JSON_Server');
+		$this->endpoint = new WP_JSON_Posts( $this->fake_server );
+
+	}
+
+	public function test_revisions_access_by_user() {
+
+		// Contributor shouldn't have access to another user's revisions
+		wp_set_current_user( $this->contributor );
+		$response = $this->endpoint->get_revisions( $this->post_id );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 403, $response->get_status() );
+
+		// Logged out users shouldn't have access to any revisions
+		wp_set_current_user( 0 );
+		$response = $this->endpoint->get_revisions( $this->post_id );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 403, $response->get_status() );
+
+		// Authors should have access to their own revisions
+		wp_set_current_user( $this->author );
+		$response = $this->endpoint->get_revisions( $this->post_id );
+		$this->assertNotInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 200, $response->get_status() );
+		$response = json_ensure_response( $response );
+		$data = $response->get_data();
+		$this->assertEquals( 1, count( $data ) );
+
+	}
+
+}
