776a901694a14b8135fd9f2ade0db2e9201274b7
Ryan McCue <me@ryanmccue.info>
2014-07-27 15:39:08 -0500

    Split comments into separate class

--- lib/class-wp-json-posts.php
+++ lib/class-wp-json-posts.php
@@ -15,6 +15,8 @@ class WP_JSON_Posts {
 	 */
 	public function __construct(WP_JSON_ResponseHandler $server) {
 		$this->server = $server;
+
+		$this->comments = new WP_JSON_Comments();
 	}
 
 	/**
@@ -51,15 +53,6 @@ class WP_JSON_Posts {
 				array( array( $this, 'delete_meta' ),    WP_JSON_Server::DELETABLE ),
 			),
 
-			// Comments
-			'/posts/(?P<id>\d+)/comments' => array(
-				array( array( $this, 'get_comments' ),   WP_JSON_Server::READABLE ),
-			),
-			'/posts/(?P<id>\d+)/comments/(?P<comment>\d+)' => array(
-				array( array( $this, 'get_comment' ),    WP_JSON_Server::READABLE ),
-				array( array( $this, 'delete_comment' ), WP_JSON_Server::DELETABLE ),
-			),
-
 			// Meta-post endpoints
 			'/posts/types' => array(
 				array( $this, 'get_post_types' ),        WP_JSON_Server::READABLE
@@ -71,6 +64,8 @@ class WP_JSON_Posts {
 				array( $this, 'get_post_statuses' ),     WP_JSON_Server::READABLE
 			),
 		);
+		$post_routes = $this->comments->register_routes( $post_routes );
+
 		return array_merge( $routes, $post_routes );
 	}
 
@@ -463,93 +458,6 @@ class WP_JSON_Posts {
 			// TODO: return a HTTP 202 here instead
 			return array( 'message' => __( 'Deleted post' ) );
 		}
-	}
-
-	/**
-	 * Delete a comment.
-	 *
-	 * @uses wp_delete_comment
-	 * @param int $id Post ID
-	 * @param int $comment Comment ID
-	 * @param boolean $force Skip trash
-	 * @return array
-	 */
-	public function delete_comment( $id, $comment, $force = false ) {
-		$comment = (int) $comment;
-
-		if ( empty( $comment ) ) {
-			return new WP_Error( 'json_comment_invalid_id', __( 'Invalid comment ID.' ), array( 'status' => 404 ) );
-		}
-
-		$comment_array = get_comment( $comment, ARRAY_A );
-
-		if ( empty( $comment_array ) ) {
-			return new WP_Error( 'json_comment_invalid_id', __( 'Invalid comment ID.' ), array( 'status' => 404 ) );
-		}
-
-		if ( ! current_user_can(  'edit_comment', $comment_array['comment_ID'] ) ) {
-			return new WP_Error( 'json_user_cannot_delete_comment', __( 'Sorry, you are not allowed to delete this comment.' ), array( 'status' => 401 ) );
-		}
-
-		$result = wp_delete_comment( $comment_array['comment_ID'], $force );
-
-		if ( ! $result ) {
-			return new WP_Error( 'json_cannot_delete', __( 'The comment cannot be deleted.' ), array( 'status' => 500 ) );
-		}
-
-		if ( $force ) {
-			return array( 'message' => __( 'Permanently deleted comment' ) );
-		} else {
-			// TODO: return a HTTP 202 here instead
-			return array( 'message' => __( 'Deleted comment' ) );
-		}
-	}
-
-	/**
-	 * Retrieve comments
-	 *
-	 * @param int $id Post ID to retrieve comments for
-	 * @return array List of Comment entities
-	 */
-	public function get_comments( $id ) {
-		//$args = array('status' => $status, 'post_id' => $id, 'offset' => $offset, 'number' => $number )l
-		$comments = get_comments( array('post_id' => $id) );
-
-		$post = get_post( $id, ARRAY_A );
-
-		if ( empty( $post['ID'] ) ) {
-			return new WP_Error( 'json_post_invalid_id', __( 'Invalid post ID.' ), array( 'status' => 404 ) );
-		}
-
-		if ( ! $this->check_read_permission( $post ) ) {
-			return new WP_Error( 'json_user_cannot_read', __( 'Sorry, you cannot read this post.' ), array( 'status' => 401 ) );
-		}
-
-		$struct = array();
-
-		foreach ( $comments as $comment ) {
-			$struct[] = $this->prepare_comment( $comment, array( 'comment', 'meta' ), 'collection' );
-		}
-
-		return $struct;
-	}
-
-	/**
-	 * Retrieve a single comment
-	 *
-	 * @param int $comment Comment ID
-	 * @return array Comment entity
-	 */
-	public function get_comment( $comment ) {
-		$comment = get_comment( $comment );
-
-		if ( empty( $comment ) ) {
-			return new WP_Error( 'json_comment_invalid_id', __( 'Invalid comment ID.' ), array( 'status' => 404 ) );
-		}
-
-		$data = $this->prepare_comment( $comment );
-
-		return $data;
 	}
 
 	/**
@@ -1515,121 +1423,5 @@ class WP_JSON_Posts {
 		do_action( 'json_insert_post', $post, $data, $update );
 
 		return $post_ID;
-	}
-
-	/**
-	 * Prepares comment data for returning as a JSON response.
-	 *
-	 * @param stdClass $comment Comment object
-	 * @param array $requested_fields Fields to retrieve from the comment
-	 * @param string $context Where is the comment being loaded?
-	 * @return array Comment data for JSON serialization
-	 */
-	protected function prepare_comment( $comment, $requested_fields = array( 'comment', 'meta' ), $context = 'single' ) {
-		$fields = array(
-			'ID'   => (int) $comment->comment_ID,
-			'post' => (int) $comment->comment_post_ID,
-		);
-
-		$post = (array) get_post( $fields['post'] );
-
-		// Content
-		$fields['content'] = apply_filters( 'comment_text', $comment->comment_content, $comment );
-		// $fields['content_raw'] = $comment->comment_content;
-
-		// Status
-		switch ( $comment->comment_approved ) {
-			case 'hold':
-			case '0':
-				$fields['status'] = 'hold';
-				break;
-
-			case 'approve':
-			case '1':
-				$fields['status'] = 'approved';
-				break;
-
-			case 'spam':
-			case 'trash':
-			default:
-				$fields['status'] = $comment->comment_approved;
-				break;
-		}
-
-		// Type
-		$fields['type'] = apply_filters( 'get_comment_type', $comment->comment_type );
-
-		if ( empty( $fields['type'] ) ) {
-			$fields['type'] = 'comment';
-		}
-
-		// Post
-		if ( 'single' === $context ) {
-			$parent = get_post( $post['post_parent'], ARRAY_A );
-			$fields['parent'] = $this->prepare_post( $parent, 'single-parent' );
-		}
-
-		// Parent
-		if ( ( 'single' === $context || 'single-parent' === $context ) && (int) $comment->comment_parent ) {
-			$parent_fields = array( 'meta' );
-
-			if ( $context === 'single' ) {
-				$parent_fields[] = 'comment';
-			}
-			$parent = get_comment( $post['post_parent'] );
-
-			$fields['parent'] = $this->prepare_comment( $parent, $parent_fields, 'single-parent' );
-		}
-
-		// Parent
-		$fields['parent'] = (int) $comment->comment_parent;
-
-		// Author
-		if ( (int) $comment->user_id !== 0 ) {
-			$fields['author'] = (int) $comment->user_id;
-		} else {
-			$fields['author'] = array(
-				'ID'     => 0,
-				'name'   => $comment->comment_author,
-				'URL'    => $comment->comment_author_url,
-				'avatar' => json_get_avatar_url( $comment->comment_author_email ),
-			);
-		}
-
-		// Date
-		$timezone = json_get_timezone();
-
-		$date               = WP_JSON_DateTime::createFromFormat( 'Y-m-d H:i:s', $comment->comment_date, $timezone );
-		$fields['date']     = $date->format( 'c' );
-		$fields['date_tz']  = $date->format( 'e' );
-		$fields['date_gmt'] = date( 'c', strtotime( $comment->comment_date_gmt ) );
-
-		// Meta
-		$meta = array(
-			'links' => array(
-				'up' => json_url( sprintf( '/posts/%d', (int) $comment->comment_post_ID ) )
-			),
-		);
-
-		if ( 0 !== (int) $comment->comment_parent ) {
-			$meta['links']['in-reply-to'] = json_url( sprintf( '/posts/%d/comments/%d', (int) $comment->comment_post_ID, (int) $comment->comment_parent ) );
-		}
-
-		if ( 'single' !== $context ) {
-			$meta['links']['self'] = json_url( sprintf( '/posts/%d/comments/%d', (int) $comment->comment_post_ID, (int) $comment->comment_ID ) );
-		}
-
-		// Remove unneeded fields
-		$data = array();
-
-		if ( in_array( 'comment', $requested_fields ) ) {
-			$data = array_merge( $data, $fields );
-		}
-
-		if ( in_array( 'meta', $requested_fields ) ) {
-			$data['meta'] = $meta;
-		}
-
-		return apply_filters( 'json_prepare_comment', $data, $comment, $context );
 	}
 }
