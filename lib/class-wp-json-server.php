e023cec6663dbed5ee621e6c1519107773ba0853
Josh Kadis <josh@alleyinteractive.com>
2014-08-01 16:36:12 -0400

    issue 203: test json_last_error() after json_encode() or json_decode()

--- lib/class-wp-json-server.php
+++ lib/class-wp-json-server.php
@@ -271,6 +271,13 @@ class WP_JSON_Server implements WP_JSON_ResponseHandler {
 
 			$result = json_encode( $this->prepare_response( $result ) );
 
+			// test for json_encode() error
+			$json_error_message = $this->get_json_last_error();
+			if ( $json_error_message ) {
+				$json_error_obj = new WP_Error( 'json_encode_error', __( 'json_encode() error: ' . $json_error_message ), array( 'status' => 400 ) );
+				$result = $this->error_to_response( $json_error_obj );
+			}
+
 			if ( isset( $_GET['_jsonp'] ) ) {
 				// Prepend '/**/' to mitigate possible JSONP Flash attacks
 				// http://miki.it/blog/2014/7/8/abusing-jsonp-with-rosetta-flash/
@@ -376,6 +383,12 @@ class WP_JSON_Server implements WP_JSON_ResponseHandler {
 				if ( $supported & self::ACCEPT_JSON ) {
 					$data = json_decode( $this->get_raw_data(), true );
 
+					// test for json_decode() error
+					$json_error_message = $this->get_json_last_error();
+					if ( $json_error_message ) {
+						return new WP_Error( 'json_decode_error', __( 'json_decode() error: ' . $json_error_message ), array( 'status' => 400 ) );
+					}
+
 					if ( $data !== null ) {
 						$args = array_merge( $args, array( 'data' => $data ) );
 					}
@@ -411,6 +424,61 @@ class WP_JSON_Server implements WP_JSON_ResponseHandler {
 		}
 
 		return new WP_Error( 'json_no_route', __( 'No route was found matching the URL and request method' ), array( 'status' => 404 ) );
+	}
+
+	/**
+	 * Returns if an error occurred during most recent JSON encode/decode
+	 *
+	 * @return boolean|string Boolean false or string error message
+	 */
+	protected function get_json_last_error( ) {
+		// see https://core.trac.wordpress.org/ticket/27799
+		if ( ! function_exists( 'json_last_error' ) ) {
+			return false;
+		}
+
+		$last_error_code = json_last_error();
+
+		// just in case JSON_ERROR_NONE is not defined
+		$error_code_none = defined( 'JSON_ERROR_NONE' ) ? JSON_ERROR_NONE : 0;
+
+		// return false if no error occurred
+		if ( $last_error_code === $error_code_none ) {
+			return false;
+		}
+
+		// use json_last_error_msg() if available
+		if ( function_exists( 'json_last_error_msg' ) ) {
+			return json_last_error_msg();
+		}
+
+		// otherwise use built-in constants
+		if ( defined( 'JSON_ERROR_DEPTH' ) && JSON_ERROR_DEPTH === $last_error_code ) {
+			return 'Maximum stack depth exceeded';
+		}
+		elseif ( defined( 'JSON_ERROR_STATE_MISMATCH' ) && JSON_ERROR_STATE_MISMATCH === $last_error_code ) {
+			return 'Invalid or malformed JSON';
+		}
+		elseif ( defined( 'JSON_ERROR_CTRL_CHAR' ) && JSON_ERROR_CTRL_CHAR === $last_error_code ) {
+			return 'Control character error, possibly incorrectly encoded';
+		}
+		elseif ( defined( 'JSON_ERROR_SYNTAX' ) && JSON_ERROR_SYNTAX === $last_error_code ) {
+			return 'Syntax error';
+		}
+		elseif ( defined( 'JSON_ERROR_UTF8' ) && JSON_ERROR_UTF8 === $last_error_code ) {
+			return 'Malformed UTF-8 characters, possibly incorrectly encoded';
+		}
+		elseif ( defined( 'JSON_ERROR_RECURSION' ) && JSON_ERROR_RECURSION === $last_error_code ) {
+			return 'One or more recursive references in the value to be encoded';
+		}
+		elseif ( defined( 'JSON_ERROR_INF_OR_NAN' ) && JSON_ERROR_INF_OR_NAN === $last_error_code ) {
+			return 'One or more NAN or INF values in the value to be encoded';
+		}
+		elseif ( defined( 'JSON_ERROR_UNSUPPORTED_TYPE' ) && JSON_ERROR_UNSUPPORTED_TYPE === $last_error_code ) {
+			return 'A value of a type that cannot be encoded was given';
+		} else {
+			return 'An unknown error occurred';
+		}
 	}
 
 	/**
