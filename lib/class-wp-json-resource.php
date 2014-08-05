02f5f4f9eadbec0850656a43d5892aedae44af16
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-05 10:52:26 -0400

    Introduce `WP_JSON_Resource` for accessing resources

---
+++ lib/class-wp-json-resource.php
@@ -0,0 +1,25 @@
+<?php
+
+abstract class WP_JSON_Resource extends jsonSerializable {
+
+	protected $data;
+
+	protected function __construct( $data ) {
+		$this->data = $data;
+	}
+
+	abstract public function get();
+	abstract public function update();
+	abstract public function delete();
+
+	public static function create() {}
+	public static function get_instance() {}
+
+	/**
+	 * Get this resource's relationships to other resources
+	 */
+	public function relationships() {
+		return array();
+	}
+
+}
