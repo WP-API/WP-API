bba7c355ac8ff3e2e25f26e58d9194067ddb87df
Daniel Bachhuber <d@danielbachhuber.com>
2014-08-05 20:23:00 -0400

    Retool tests so current user context is more explicit
    Also adds tests where the request is a logged-out request

--- tests/test-json-users.php
+++ tests/test-json-users.php
@@ -10,12 +10,12 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 	public function setUp() {
 		parent::setUp();
 
-		$this->user = $this->factory->user->create();
-		wp_set_current_user( $this->user );
-		$this->user_obj = wp_get_current_user();
-
 		$this->subscriber = $this->factory->user->create( array( 'role' => 'subscriber' ) );
+		$this->subscriber_obj = get_user_by( 'id', $this->subscriber );
+		$this->author = $this->factory->user->create( array( 'role' => 'author' ) );
+		$this->author_obj = get_user_by( 'id', $this->author );
 		$this->administrator = $this->factory->user->create( array( 'role' => 'administrator' ) );
+		$this->administrator_obj = get_user_by( 'id', $this->administrator );
 
 		$this->fake_server = $this->getMock('WP_JSON_Server');
 		$this->endpoint = new WP_JSON_Users( $this->fake_server );
@@ -27,44 +27,6 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 		} else {
 			$user->set_role( 'administrator' );
 		}
-	}
-
-	public function test_get_current_user() {
-		$response = $this->endpoint->get_current_user();
-		$this->assertNotInstanceOf( 'WP_Error', $response );
-		$response = json_ensure_response( $response );
-
-		// Check that we succeeded
-		$this->assertEquals( 302, $response->get_status() );
-
-		$headers = $response->get_headers();
-		$response_data = $response->get_data();
-		$this->assertArrayHasKey( 'Location', $headers );
-
-		$this->check_get_user_response( $response, $this->user_obj );
-
-	}
-
-	public function test_get_user() {
-		$response = $this->endpoint->get_user( $this->user );
-		$this->assertNotInstanceOf( 'WP_Error', $response );
-		$response = json_ensure_response( $response );
-
-		// Check that we succeeded
-		$this->assertEquals( 200, $response->get_status() );
-
-		$this->check_get_user_response( $response, $this->user_obj );
-	}
-
-	public function test_get_user_with_edit_context() {
-		$response = $this->endpoint->get_user( $this->user, 'edit' );
-		$this->assertNotInstanceOf( 'WP_Error', $response );
-		$response = json_ensure_response( $response );
-
-		// Check that we succeeded
-		$this->assertEquals( 200, $response->get_status() );
-
-		$this->check_get_user_response( $response, $this->user_obj, 'edit' );
 	}
 
 	protected function check_get_user_response( $response, $user_obj, $context = 'view' ) {
@@ -83,34 +45,85 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 		}
 	}
 
-	public function test_create_user() {
-		$this->allow_user_to_create_users( $this->user_obj );
+	public function test_get_current_user_logged_out() {
+		wp_set_current_user( 0 );
+		$response = $this->endpoint->get_current_user();
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 'json_not_logged_in', $response->get_error_code() );
+	}
+
+	public function test_get_current_user_logged_in() {
+		wp_set_current_user( $this->author );
+		$response = $this->endpoint->get_current_user();
+		$this->assertNotInstanceOf( 'WP_Error', $response );
+		$response = json_ensure_response( $response );
+		$this->assertEquals( 302, $response->get_status() );
+		$headers = $response->get_headers();
+		$response_data = $response->get_data();
+		$this->assertArrayHasKey( 'Location', $headers );
+		$this->check_get_user_response( $response, $this->author_obj, 'view' );
+	}
+
+	public function test_get_user_logged_out_view_only() {
+		wp_set_current_user( 0 );
+		// edit context
+		$response = $this->endpoint->get_user( $this->author, 'edit' );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 'json_user_cannot_edit', $response->get_error_code() );
+		// view-private context
+		$response = $this->endpoint->get_user( $this->author, 'view-private' );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 'json_user_cannot_view', $response->get_error_code() );
+		// view context
+		$response = $this->endpoint->get_user( $this->author, 'view' );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 'json_user_cannot_view', $response->get_error_code() );
+	}
+
+	public function get_user_logged_in_edit_context() {
+		wp_set_current_user( $this->author );
+		$response = $this->endpoint->get_user( $this->user, 'edit' );
+		$this->assertNotInstanceOf( 'WP_Error', $response );
+		$response = json_ensure_response( $response );
+
+		$this->assertEquals( 200, $response->get_status() );
+		$this->check_get_user_response( $response, $this->author_obj, 'edit' );
+	}
+
+	public function test_create_user_logged_out() {
+		wp_set_current_user( 0 );
 		$data = array(
 			'username' => 'test_user',
 			'password' => 'test_password',
 			'email' => 'test@example.com',
 		);
 		$response = $this->endpoint->create_user( $data );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 'json_cannot_create', $response->get_error_code() );
+	}
 
-		// Check that we succeeded
+	public function test_create_user_logged_in() {
+		wp_set_current_user( $this->administrator );
+		$this->allow_user_to_create_users( $this->administrator_obj );
+		$data = array(
+			'username' => 'test_user',
+			'password' => 'test_password',
+			'email' => 'test@example.com',
+		);
+		$response = $this->endpoint->create_user( $data );
 		$this->assertNotInstanceOf( 'WP_Error', $response );
 		$this->assertEquals( 201, $response->get_status() );
-
 		$response_data = $response->get_data();
-
-		// Check that the data is intact
 		$new_user = get_userdata( $response_data['id'] );
-
 		$this->assertEquals( $data['username'], $response_data['username'] );
 		$this->assertEquals( $data['username'], $new_user->user_login );
-
 		$this->assertEquals( $data['email'], $new_user->user_email );
-
 		$this->assertTrue( wp_check_password( $data['password'], $new_user->user_pass ), 'Password check failed' );
 	}
 
 	public function test_create_user_missing_params() {
-		$this->allow_user_to_create_users( $this->user_obj );
+		wp_set_current_user( $this->administrator );
+		$this->allow_user_to_create_users( $this->administrator_obj );
 		$data = array(
 			'username' => 'test_user',
 		);
@@ -119,7 +132,8 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 	}
 
 	public function test_delete_user() {
-		$this->allow_user_to_create_users( $this->user_obj );
+		wp_set_current_user( $this->administrator );
+		$this->allow_user_to_create_users( $this->administrator_obj );
 
 		// Test with a new user, rather than ourselves, to avoid any
 		// complications with doing so. We should check this separately though.
@@ -134,7 +148,8 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 	}
 
 	public function test_delete_user_reassign() {
-		$this->allow_user_to_create_users( $this->user_obj );
+		wp_set_current_user( $this->administrator );
+		$this->allow_user_to_create_users( $this->administrator_obj );
 
 		// Test with a new user, rather than ourselves, to avoid any
 		// complications with doing so. We should check this separately though.
@@ -163,12 +178,13 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 	}
 
 	public function test_update_user() {
-		$pw_before = $this->user_obj->user_pass;
+		wp_set_current_user( $this->administrator );
+		$pw_before = $this->administrator_obj->user_pass;
 
 		$data = array(
 			'first_name' => 'New Name',
 		);
-		$response = $this->endpoint->edit_user( $this->user, $data );
+		$response = $this->endpoint->edit_user( $this->administrator, $data );
 		$this->assertNotInstanceOf( 'WP_Error', $response );
 		$response = json_ensure_response( $response );
 
@@ -179,7 +195,7 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 		$new_data = $response->get_data();
 		$this->assertEquals( $data['first_name'], $new_data['first_name'] );
 
-		$user = get_userdata( $this->user );
+		$user = get_userdata( $this->administrator );
 		$this->assertEquals( $user->first_name, $data['first_name'] );
 
 		// Check that we haven't inadvertently changed the user's password,
@@ -187,30 +203,25 @@ class WP_Test_JSON_User extends WP_UnitTestCase {
 		$this->assertEquals( $pw_before, $user->user_pass );
 	}
 
-	public function test_get_users() {
-
-		/*
-		 * Logged out has no permission to view users
-		 */
+	public function test_get_users_logged_out() {
 		wp_set_current_user( 0 );
 		$response = $this->endpoint->get_users();
 		$this->assertInstanceOf( 'WP_Error', $response );
 		$this->assertEquals( 'json_user_cannot_list', $response->get_error_code() );
+	}
 
-		/*
-		 * Logged-in subscriber can't view users
-		 */
+	public function test_get_users_logged_in_subscriber() {
 		wp_set_current_user( $this->subscriber );
 		$response = $this->endpoint->get_users();
 		$this->assertInstanceOf( 'WP_Error', $response );
 		$this->assertEquals( 'json_user_cannot_list', $response->get_error_code() );
+	}
 
-		/*
-		 * Logged-in admin can view users
-		 */
+	public function test_get_users_logged_in_administrator() {
 		wp_set_current_user( $this->administrator );
 		$response = $this->endpoint->get_users();
 		$this->assertNotInstanceOf( 'WP_Error', $response );
 
 	}
+
 }
