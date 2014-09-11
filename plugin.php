931eef820608d7203d4be26850f36c343cb3fe4a
Rachel Baker <rachel@rachelbaker.me>
2014-09-10 19:37:09 -0500

    Add json_check_delete_permission function.

--- plugin.php
+++ plugin.php
@@ -582,6 +582,27 @@ function json_check_create_permission( $object, $type = 'post' ) {
 }
 
 /**
+ * Check if we have permissions to delete an object.
+ *
+ * @param object $object Post|User|Term object.
+ * @param string $type Slug of object being passed.
+ * @return boolean Can we delete it?
+ */
+function json_check_delete_permission( $object, $type = 'post' ) {
+	$permission = false;
+
+	if ( 'post' == $type ) {
+		$post_type  = get_post_type_object( $object['post_type'] );
+
+		if ( current_user_can( $post_type->cap->delete_post, $object['ID'] ) ) {
+			$permission = true;
+		}
+	}
+
+	return apply_filters( "json_delete_{$type}_permission", $permission, $object );
+}
+
+/**
  * Parse an RFC3339 timestamp into a DateTime.
  *
  * @param string $date      RFC3339 timestamp.
