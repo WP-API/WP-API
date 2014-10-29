ea0639790513be87084b4a178f28927277ce4b9b
Weston Ruter <weston@xwp.co>
2014-10-29 16:59:12 -0700

    Add PHP syntax check and PHP_CodeSniffer to Travis

---
+++ bin/ci-env.sh
@@ -0,0 +1,6 @@
+export PHPCS_DIR=/tmp/phpcs
+export PHPCS_GITHUB_SRC=squizlabs/PHP_CodeSniffer
+export PHPCS_GIT_TREE=phpcs-fixer # this is the 2.0RC version of PHPCS
+export WPCS_GITHUB_SRC=WordPress-Coding-Standards/WordPress-Coding-Standards
+export WPCS_GIT_TREE=develop
+export WPCS_STANDARD=WordPress-Core
