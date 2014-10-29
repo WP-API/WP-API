ddf3d0b2c596638f08db7f26cc4208c41bb6c95e
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-29 15:34:49 -0700

    We're passing `WP_JSON_Request`, new class TBD

--- lib/class-wp-json-controller.php
+++ lib/class-wp-json-controller.php
@@ -7,25 +7,25 @@ abstract class WP_JSON_Controller {
 	 * Get a collection of items
 	 *
 	 * @param array $args (optional) Arguments supplied in the request
-	 * @param WP_HTTP_Request $request Full data about the request
+	 * @param WP_JSON_Request $request Full data about the request
 	 * @return array|WP_Error
 	 */
-	abstract public function get_items( array $args, WP_HTTP_Request $request );
+	abstract public function get_items( array $args, WP_JSON_Request $request );
 
 	/**
 	 * Get one item from the collection
 	 */
-	abstract public function get_item( array $args, WP_HTTP_Request $request );
+	abstract public function get_item( array $args, WP_JSON_Request $request );
 
 	/**
 	 * Update one item from the collection
 	 */
-	abstract public function update_item( array $args, WP_HTTP_Request $request );
+	abstract public function update_item( array $args, WP_JSON_Request $request );
 
 	/**
 	 * Delete one item from the collection
 	 */
-	abstract public function delete_item( array $args, WP_HTTP_Request $request );
+	abstract public function delete_item( array $args, WP_JSON_Request $request );
 
 	/**
 	 * Prepare the item for the JSON response
