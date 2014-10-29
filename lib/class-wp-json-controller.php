9a7ee19c1221a4d74b2c39ac11329fd388f5bc01
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-29 15:27:41 -0700

    Mark these methods explicitly abstract
    They're intended to be overridden

--- lib/class-wp-json-controller.php
+++ lib/class-wp-json-controller.php
@@ -10,22 +10,22 @@ abstract class WP_JSON_Controller {
 	 * @param WP_HTTP_Request $request Full data about the request
 	 * @return array|WP_Error
 	 */
-	public function get_items( array $args, WP_HTTP_Request $request );
+	abstract public function get_items( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Get one item from the collection
 	 */
-	public function get_item( array $args, WP_HTTP_Request $request );
+	abstract public function get_item( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Update one item from the collection
 	 */
-	public function update_item( array $args, WP_HTTP_Request $request );
+	abstract public function update_item( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Delete one item from the collection
 	 */
-	public function delete_item( array $args, WP_HTTP_Request $request );
+	abstract public function delete_item( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Prepare the item for the JSON response
@@ -33,6 +33,6 @@ abstract class WP_JSON_Controller {
 	 * @param mixed $item WordPress representation of the item
 	 * @return object
 	 */
-	public static function prepare_item_for_response( $item );
+	abstract public static function prepare_item_for_response( $item );
 
 }
