3f8811318fb560bf2c19b25ce9ce33a92d2c38d5
Daniel Bachhuber <d@danielbachhuber.com>
2014-10-28 21:18:30 -0700

    Clarify that our new request object is always passed

--- lib/class-wp-json-resource.php
+++ lib/class-wp-json-resource.php
@@ -5,23 +5,27 @@ abstract class WP_JSON_Resource {
 
 	/**
 	 * Get a collection of items
+	 *
+	 * @param array $args (optional) Arguments supplied in the request
+	 * @param WP_HTTP_Request $request Full data about the request
+	 * @return array|WP_Error
 	 */
-	public function get_items( array $args, WP_HTTP_Response $response );
+	public function get_items( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Get one item from the collection
 	 */
-	public function get_item( array $args );
+	public function get_item( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Update one item from the collection
 	 */
-	public function update_item( array $args );
+	public function update_item( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Delete one item from the collection
 	 */
-	public function delete_item( array $args );
+	public function delete_item( array $args, WP_HTTP_Request $request );
 
 	/**
 	 * Prepare the item for the JSON response
