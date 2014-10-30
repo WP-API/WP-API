7a0311f174f4bedc37da400539fbdc994dbdd942
Weston Ruter <weston@xwp.co>
2014-10-29 17:07:26 -0700

    Switch from develop to master branch for WPCS

--- bin/ci-env.sh
+++ bin/ci-env.sh
@@ -3,5 +3,5 @@ export PHPCS_GITHUB_SRC=squizlabs/PHP_CodeSniffer
 export PHPCS_GIT_TREE=phpcs-fixer # this is the 2.0RC version of PHPCS
 export WPCS_DIR=/tmp/wpcs
 export WPCS_GITHUB_SRC=WordPress-Coding-Standards/WordPress-Coding-Standards
-export WPCS_GIT_TREE=develop
+export WPCS_GIT_TREE=master
 export WPCS_STANDARD=WordPress-Core
