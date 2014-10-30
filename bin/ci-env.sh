05354f57bca31729bd5ad9731ea463e8b127abbb
Weston Ruter <weston@xwp.co>
2014-10-29 17:03:50 -0700

    Add missing WPCS_DIR var to ci-env.sh

--- bin/ci-env.sh
+++ bin/ci-env.sh
@@ -1,6 +1,7 @@
 export PHPCS_DIR=/tmp/phpcs
 export PHPCS_GITHUB_SRC=squizlabs/PHP_CodeSniffer
 export PHPCS_GIT_TREE=phpcs-fixer # this is the 2.0RC version of PHPCS
+export WPCS_DIR=/tmp/wpcs
 export WPCS_GITHUB_SRC=WordPress-Coding-Standards/WordPress-Coding-Standards
 export WPCS_GIT_TREE=develop
 export WPCS_STANDARD=WordPress-Core
