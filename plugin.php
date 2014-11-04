70b5a07fd86e1ed6e29fd80ca7aa1dd3494ed336
NikV <techvoltz@gmail.com>
2014-11-03 18:59:07 -0600

    Deleting JS Loader from Plugin.php
    Moved to extras.php

--- plugin.php
+++ plugin.php
@@ -276,19 +276,6 @@ function json_api_deactivation( $network_wide ) {
 }
 register_deactivation_hook( __FILE__, 'json_api_deactivation' );
 
-/**
- * Register API Javascript helpers.
- *
- * @see wp_register_scripts()
- */
-function json_register_scripts() {
-	wp_register_script( 'wp-api', 'http://wp-api.github.io/client-js/build/js/wp-api.js', array( 'jquery', 'backbone', 'underscore' ), '1.1', true );
-
-	$settings = array( 'root' => esc_url_raw( get_json_url() ), 'nonce' => wp_create_nonce( 'wp_json' ) );
-	wp_localize_script( 'wp-api', 'WP_API_Settings', $settings );
-}
-add_action( 'wp_enqueue_scripts', 'json_register_scripts', -100 );
-add_action( 'admin_enqueue_scripts', 'json_register_scripts', -100 );
 
 /**
  * Add the API URL to the WP RSD endpoint.
