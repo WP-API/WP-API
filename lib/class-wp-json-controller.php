d3278b73ce9dc28eedb2704981b032d78b4ebfd7
Daniel Bachhuber <d@danielbachhuber.com>
2014-11-04 17:55:29 -0800

    Avoid strict notices by making these requirements looser

--- lib/class-wp-json-controller.php
+++ lib/class-wp-json-controller.php
@@ -9,28 +9,28 @@ abstract class WP_JSON_Controller {
 	 * @param WP_JSON_Request $request Full data about the request
 	 * @return array|WP_Error
 	 */
-	public function get_items( WP_JSON_Request $request ) {
+	public function get_items( $request ) {
 		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
 	}
 
 	/**
 	 * Get one item from the collection
 	 */
-	public function get_item( WP_JSON_Request $request ) {
+	public function get_item( $request ) {
 		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
 	}
 
 	/**
 	 * Update one item from the collection
 	 */
-	public function update_item( WP_JSON_Request $request ) {
+	public function update_item( $request ) {
 		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
 	}
 
 	/**
 	 * Delete one item from the collection
 	 */
-	public function delete_item( WP_JSON_Request $request ) {
+	public function delete_item( $request ) {
 		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
 	}
 
@@ -40,7 +40,7 @@ abstract class WP_JSON_Controller {
 	 * @param mixed $item WordPress representation of the item
 	 * @return object
 	 */
-	public function prepare_item_for_response( $item ) {
+	public function prepare_item_for_response( $item, $request ) {
 		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
 	}
 
--- lib/class-wp-json-taxonomy-terms-controller.php
+++ lib/class-wp-json-taxonomy-terms-controller.php
@@ -11,7 +11,7 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 	 * @param WP_JSON_Request $request Full details about the request
 	 * @return array|WP_Error
 	 */
-	public function get_items( WP_JSON_Request $request ) {
+	public function get_items( $request ) {
 		$prepared_args = array();
 		$prepared_args['number'] = isset( $request['per_page'] ) ? (int) $request['per_page'] : 10;
 		$prepared_args['offset'] = isset( $request['page'] ) ? ( absint( $request['page'] ) - 1 ) * $prepared_args['number'] : 0; 
@@ -24,7 +24,7 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 		}
 		
 		foreach( $terms as &$term ) { 
-			$term = self::prepare_item_for_response( $term );
+			$term = self::prepare_item_for_response( $term, $request );
 		}
 		return $terms;
 	}
@@ -35,7 +35,7 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 	 * @param WP_JSON_Request $request Full details about the request
 	 * @return array|WP_Error
 	 */
-	public function get_item( WP_JSON_Request $request ) {
+	public function get_item( $request ) {
 		// Get term by does a taxonomy check for us
 		$term = get_term_by( 'id', $request['id'], $request['taxonomy'] ); 
 		if ( ! $term ) {
@@ -44,7 +44,7 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 		if ( is_wp_error( $term ) ) {
 			return $term;
 		}
-		return self::prepare_item_for_response( $term );
+		return self::prepare_item_for_response( $term, $request );
 	}
 
 	/**
@@ -53,7 +53,7 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 	 * @param WP_JSON_Request $request Full details about the request
 	 * @return array|WP_Error
 	 */
-	public function update_item( WP_JSON_Request $request ) {
+	public function update_item( $request ) {
 		$prepared_args = array();
 		if ( isset( $request['name'] ) ) {
 			$prepared_args['name'] = sanitize_text_field( $request['name'] );
@@ -84,7 +84,7 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 	 * @param WP_JSON_Request $request Full details about the request
 	 * @return array|WP_Error
 	 */
-	public function delete_item( WP_JSON_Request $request ) {
+	public function delete_item( $request ) {
 		
 		$term = self::get_item( array( 'id' => $request['id'], 'taxonomy' => $request['taxonomy'] ), $request );
 		if ( is_wp_error( $term ) ) {
@@ -99,8 +99,9 @@ class WP_JSON_Taxonomy_Terms_Controller extends WP_JSON_Controller {
 	 * Prepare a single term output for response
 	 *
 	 * @param obj $item Term object
+	 * @param WP_JSON_Request $request
 	 */
-	public function prepare_item_for_response( $item ) {
+	public function prepare_item_for_response( $item, $request ) {
 		return array(
 			'id'           => (int) $item->term_id,
 			'description'  => $item->description,
