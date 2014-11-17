915750746417a1007d942c13538ced8f625d65ae
Rachel Baker <rachel@rachelbaker.me>
2014-11-17 10:15:06 -0600

    Unit tests for delete_item method from users controller.

--- tests/test-json-users-controller.php
+++ tests/test-json-users-controller.php
@@ -50,4 +50,46 @@ class WP_Test_JSON_Users_Controller extends WP_Test_JSON_TestCase {
 		$response = json_ensure_response( $response );
 		$this->assertEquals( 200, $response->get_status() );
 	}
+
+	public function test_delete_user() {
+		$user_id = $this->factory->user->create();
+		wp_set_current_user( $this->user );
+
+		$request = new WP_JSON_Request;
+		$request->set_param( 'id', $user_id );
+		$response = $this->endpoint->delete_item( $request );
+		$this->assertNotInstanceOf( 'WP_Error', $response );
+		$response = json_ensure_response( $response );
+		$this->assertEquals( 200, $response->get_status() );
+	}
+
+	public function test_delete_user_reassign() {
+		// Test with a new user, to avoid any complications
+		$user_id = $this->factory->user->create();
+		$reassign_id = $this->factory->user->create();
+		$test_post = $this->factory->post->create(array(
+			'post_author' => $user_id,
+		));
+
+		// Sanity check to ensure the factory created the post correctly
+		$post = get_post( $test_post );
+		$this->assertEquals( $user_id, $post->post_author );
+
+		// Delete our test user, and reassign to the new author
+		wp_set_current_user( $this->user );
+		$request = new WP_JSON_Request;
+		$request->set_param( 'id', $user_id );
+		$request->set_param( 'reassign', $reassign_id );
+		$response = $this->endpoint->delete_item( $request );
+
+		$this->assertNotInstanceOf( 'WP_Error', $response );
+		$response = json_ensure_response( $response );
+
+		// Check that we succeeded
+		$this->assertEquals( 200, $response->get_status() );
+
+		// Check that the post has been updated correctly
+		$post = get_post( $test_post );
+		$this->assertEquals( $reassign_id, $post->post_author );
+	}
 }
