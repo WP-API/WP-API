3e0054a0bd0959dc57457efd5c035f897d61d156
Joe Hoyle <joehoyle@gmail.com>
2014-11-17 17:43:46 -0500

    Add helper methods to the json server spy class so we can capture and retreive sent headers

--- tests/spy-json-server.php
+++ tests/spy-json-server.php
@@ -1,6 +1,8 @@
 <?php
 
 class WP_Test_Spy_JSON_Server extends WP_JSON_Server {
+
+	protected $sent_headers = array();
 	/**
 	 * Get the raw $endpoints data from the server
 	 *
@@ -8,5 +10,28 @@ class WP_Test_Spy_JSON_Server extends WP_JSON_Server {
 	 */
 	public function get_raw_endpoint_data() {
 		return $this->endpoints;
+	}
+
+	/**
+	 * Get headers that have been sent with the JSON Server
+	 * 
+	 * @return array<map>
+	 */
+	public function get_sent_headers() {
+		return $this->sent_headers;
+	}
+
+	/**
+	 * Override the send_header() method to capture them instead of outputting in unit tests
+	 * 
+	 * @param  string $key
+	 * @param  string $value
+	 */
+	protected function send_header( $key, $value ) {
+
+		$this->sent_headers[$key] = $value;
+
+		// don't actually output the header
+		return;
 	}
 }
