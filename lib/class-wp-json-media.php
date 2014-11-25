845fa2e5cda66deb78b98cc6856e9eb90e1446cd
Kenny Ki <knyki.12@gmail.com>
2014-11-25 11:16:19 +0800

    Do not proceed if an error is returned

--- lib/class-wp-json-media.php
+++ lib/class-wp-json-media.php
@@ -456,6 +456,13 @@ class WP_JSON_Media extends WP_JSON_Posts {
 		if ( ! empty( $data['featured_image'] ) ) {
 			// Already verified in preinsert_check()
 			$thumbnail = $this->get_post( $data['featured_image'], 'child' );
+
+			if ( is_wp_error( $thumbnail ) ) {
+				// Since this function is not expected to return any response, add error log for debugging purpose
+				error_log('Failed to get featured_image: ' . $data['featured_image']);
+				return;
+			}
+
 			$thumbnail = json_ensure_response($thumbnail);
 			$thumbnailData = $thumbnail->get_data();
 
