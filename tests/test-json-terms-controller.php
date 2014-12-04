9e260a8a104d516ca7f99f159d231b98dc048d82
Daniel Bachhuber <d@danielbachhuber.com>
2014-12-03 17:28:23 -0800

    Fix copy pasta, which means this won't hit the wrong callback

--- tests/test-json-terms-controller.php
+++ tests/test-json-terms-controller.php
@@ -110,7 +110,7 @@ class WP_Test_JSON_Terms_Controller extends WP_Test_JSON_Controller_Testcase {
 
 	public function test_update_item_invalid_taxonomy() {
 		wp_set_current_user( $this->administrator );
-		$request = new WP_JSON_Request( 'POST', '/wp/terms/invalid-taxonomy' );
+		$request = new WP_JSON_Request( 'POST', '/wp/terms/invalid-taxonomy/9999999' );
 		$request->set_param( 'name', 'Invalid Taxonomy' );
 		$response = $this->server->dispatch( $request );
 		$this->assertErrorResponse( 'json_taxonomy_invalid', $response, 404 );
