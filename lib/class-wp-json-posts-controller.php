54b02aac6deca6e45c39035831f2b9f22175a76c
Rachel Baker <rachel@rachelbaker.me>
2014-12-22 12:12:03 -0600

    Move get_item_revisions method into the base post class.

--- lib/class-wp-json-posts-controller.php
+++ lib/class-wp-json-posts-controller.php
@@ -6,42 +6,6 @@
 class WP_JSON_Posts_Controller extends WP_JSON_Base_Posts_Controller {
 
 	/**
-	 * Get revisions for a specific post.
-	 *
-	 * @param WP_JSON_Request $request Full details about the request
-	 * @return WP_Error|WP_HTTP_ResponseInterface
-	 */
-	public function get_item_revisions( $request ) {
-		$request->set_query_params( array(
-			'context' => 'view-revision',
-		) );
-
-		$id = (int) $request['id'];
-		$parent = get_post( $id );
-
-		if ( empty( $id ) || empty( $parent->ID ) ) {
-			return new WP_Error( 'json_post_invalid_id', __( 'Invalid post ID.' ), array( 'status' => 404 ) );
-		}
-
-		if ( ! $this->check_edit_permission( $parent ) ) {
-			return new WP_Error( 'json_cannot_view', __( 'Sorry, you cannot view the revisions for this post.' ), array( 'status' => 403 ) );
-		}
-
-		// Todo: Query args filter for wp_get_post_revisions
-		$revisions = wp_get_post_revisions( $id );
-
-		$struct = array();
-		foreach ( $revisions as $revision ) {
-			$struct[] = $this->prepare_item_for_response( $revision, $request );
-		}
-
-		$response = json_ensure_response( $struct );
-		$response->link_header( 'alternate',  get_permalink( $id ), array( 'type' => 'text/html' ) );
-
-		return $response;
-	}
-
-	/**
 	 * Prepare a single post output for response
 	 *
 	 * @param WP_Post $post Post object
