dae749cb53f3a07b523e6bc54d4c5377ec4dd310
Rachel Baker <rachel@rachelbaker.me>
2015-01-02 13:23:07 -0600

    Add periods to end of tag descriptions in base controller as well.

--- lib/class-wp-json-controller.php
+++ lib/class-wp-json-controller.php
@@ -6,8 +6,8 @@ abstract class WP_JSON_Controller {
 	/**
 	 * Get a collection of items
 	 *
-	 * @param WP_JSON_Request $request Full data about the request
-	 * @return mixed WP_Error or WP_JSON_Response
+	 * @param WP_JSON_Request $request Full data about the request.
+	 * @return mixed WP_Error or WP_JSON_Response.
 	 */
 	public function get_items( $request ) {
 		return new WP_Error( 'invalid-method', __( "Method not implemented. Must be over-ridden in subclass." ), array( 'status' => 405 ) );
@@ -44,8 +44,8 @@ abstract class WP_JSON_Controller {
 	/**
 	 * Prepare the item for the JSON response
 	 *
-	 * @param mixed $item WordPress representation of the item
-	 * @param WP_JSON_Request $request
+	 * @param mixed $item WordPress representation of the item.
+	 * @param WP_JSON_Request $request Request object.
 	 * @return mixed
 	 */
 	public function prepare_item_for_response( $item, $request ) {
