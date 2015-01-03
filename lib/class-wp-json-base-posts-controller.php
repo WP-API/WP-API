40d984e9aca50ab7ee6023c12ae2324a81d2c4a4
Rachel Baker <rachel@rachelbaker.me>
2015-01-03 16:12:24 -0600

    Only check if a post_type is being changed if it has been set.

--- lib/class-wp-json-base-posts-controller.php
+++ lib/class-wp-json-base-posts-controller.php
@@ -199,7 +199,7 @@ abstract class WP_JSON_Base_Posts_Controller extends WP_JSON_Controller {
 			return new WP_Error( 'json_post_cannot_edit', __( 'Sorry, you are not allowed to edit this post.' ), array( 'status' => 403 ) );
 		}
 
-		if ( $request['type'] != $post->post_type ) {
+		if ( isset( $request['type'] ) && $request['type'] != $post->post_type ) {
 			return new WP_Error( 'json_cannot_change_post_type', __( 'The post type may not be changed.' ), array( 'status' => 400 ) );
 		}
 
