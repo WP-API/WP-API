e823b095d2b371ba7d3903f1668bfc6b7106829a
Rachel Baker <rachel@rachelbaker.me>
2015-01-12 22:32:01 -0600

    Add unit tests for error responses of get_item method.

--- tests/test-json-comments-controller.php
+++ tests/test-json-comments-controller.php
@@ -20,17 +20,32 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 			'role' => 'subscriber',
 		) );
 
+		$this->post_id = $this->factory->post->create();
+
 		$this->comments[] = $this->factory->comment->create( array(
 			'comment_approved' => 1,
 			'comment_content'  => 'Approved comment',
-			'comment_post_ID'  => 1
+			'comment_post_ID'  => $this->post_id,
+		));
+
+		$this->comments[] = $this->factory->comment->create( array(
+			'comment_approved' => 1,
+			'comment_post_ID'  => $this->post_id,
+			'user_id'          => $this->subscriber,
+		));
+
+		$this->comments[] = $this->factory->comment->create( array(
+			'comment_approved' => 1,
+			'comment_post_ID'  => $this->post_id,
 		));
 
 		$this->comments[] = $this->factory->comment->create( array(
 			'comment_approved' => 0,
 			'comment_content'  => 'Unapproved comment',
-			'comment_post_ID'  => 2
+			'comment_post_ID'  => $this->post_id,
+			'user_id'          => $this->subscriber,
 		));
+
 	}
 
 	public function tearDown() {
@@ -48,17 +63,52 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 	public function test_get_items() {
 		$request = new WP_JSON_Request( 'GET', '/wp/comments' );
 		$response = $this->server->dispatch( $request );
-		
+
 		$this->assertEquals( 200, $response->get_status() );
 		$comments = $response->get_data();
-		$this->assertEquals( 1, count( $comments ) );
+		$this->assertEquals( 3, count( $comments ) );
 	}
 
 	public function test_get_item() {
 		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $this->comments[0] );
 
 		$response = $this->server->dispatch( $request );
-		
+
+		$this->assertEquals( 200, $response->get_status() );
+		$comment = $response->get_data();
+	}
+
+	public function test_get_item_invalid_id() {
+		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . 100 );
+
+		$response = $this->server->dispatch( $request );
+		$this->assertErrorResponse( 'json_comment_invalid_id', $response, 404 );
+	}
+
+	public function test_get_item_invalid_post_id() {
+		$comment_id = $this->factory->comment->create( array(
+			'comment_approved' => 1,
+			'comment_post_ID'  => 100,
+		));
+		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $comment_id );
+
+		$response = $this->server->dispatch( $request );
+		$this->assertErrorResponse( 'json_post_invalid_id', $response, 404 );
+	}
+
+	public function test_get_item_not_approved() {
+		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $this->comments[3] );
+
+		$response = $this->server->dispatch( $request );
+		$this->assertErrorResponse( 'json_user_cannot_read', $response, 401 );
+	}
+
+	public function test_get_item_not_approved_with_user() {
+		wp_set_current_user( $this->subscriber );
+
+		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $this->comments[3] );
+
+		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 		$comment = $response->get_data();
 	}
