82efb824b57a3abe91c4b552c0aea8e2d6e11b5d
Rachel Baker <rachel@rachelbaker.me>
2015-01-14 14:47:32 -0600

    Clean up the prepare_item_for_response method and abstract some logic out.

--- lib/class-wp-json-comments-controller.php
+++ lib/class-wp-json-comments-controller.php
@@ -122,65 +122,34 @@ class WP_JSON_Comments_Controller extends WP_JSON_Controller {
 	 */
 	public function prepare_item_for_response( $comment, $request ) {
 		$fields = array(
-			'id'      => (int) $comment->comment_ID,
-			'post_id' => (int) $comment->comment_post_ID,
+			'id'           => (int) $comment->comment_ID,
+			'post_id'      => (int) $comment->comment_post_ID,
+			'parent_id'    => (int) $comment->comment_parent,
+			'user_id'      => (int) $comment->user_id,
+			'author'       => $comment->comment_author,
+			'author_email' => $comment->comment_author_email,
+			'author_url'   => $comment->comment_author_url,
+			'date'         => json_mysql_to_rfc3339( $comment->comment_date ),
+			'date_gmt'     => json_mysql_to_rfc3339( $comment->comment_date_gmt ),
+			'content'      => array(
+				'rendered'     => apply_filters( 'comment_text', $comment->comment_content, $comment ),
+			),
+			'status'       => $this->prepare_status_response( $comment->comment_approved ),
+			'type'         => $this->prepare_type_response( $comment->comment_type ),
 		);
+
+		if ( 'edit' == $request['context'] ) {
+			$fields['content']['raw'] = $comment->comment_content;
+		}
 
 		$links = array();
-
-		$context = isset( $request['context'] ) ? $request['context'] : '';
-
-		// Content
-		$fields['content'] = array(
-			'rendered' => apply_filters( 'comment_text', $comment->comment_content, $comment )
-		);
-		// $fields['content']['raw'] = $comment->comment_content;
-
-		// Status
-		switch ( $comment->comment_approved ) {
-			case 'hold':
-			case '0':
-				$fields['status'] = 'hold';
-				break;
-
-			case 'approve':
-			case '1':
-				$fields['status'] = 'approved';
-				break;
-
-			case 'spam':
-			case 'trash':
-			default:
-				$fields['status'] = $comment->comment_approved;
-				break;
-		}
-
-		// Type
-		$fields['type'] = apply_filters( 'get_comment_type', $comment->comment_type );
-
-		if ( empty( $fields['type'] ) ) {
-			$fields['type'] = 'comment';
-		}
-
-		// Parent
-		if ( (int) $comment->comment_parent ) {
-			$links['parent'] = array(
-				'href' => json_url( '/wp/comments/' . $comment->comment_parent )
-			);
-		}
 
 		// Author
 		if ( 0 !== (int) $comment->user_id ) {
 			$links['author'] = array(
 				'href' => json_url( '/wp/users/' . $comment->user_id )
 			);
-		} else {
-			// to do handle comment with no auther user
 		}
-
-		// Date
-		$fields['date']     = json_mysql_to_rfc3339( $comment->comment_date );
-		$fields['date_gmt'] = json_mysql_to_rfc3339( $comment->comment_date_gmt );
 
 		if ( 0 !== (int) $comment->comment_parent ) {
 			$links['in-reply-to'] = array(
@@ -193,7 +162,51 @@ class WP_JSON_Comments_Controller extends WP_JSON_Controller {
 		return apply_filters( 'json_prepare_comment', $fields, $comment, $request );
 	}
 
+	/**
+	 * Check comment_approved to set comment status for single comment output.
+	 *
+	 * @param string|int $comment_approved
+	 * @return string    $status
+	 */
+	protected function prepare_status_response( $comment_approved ) {
+		$status = '';
 
+		switch ( $comment_approved ) {
+			case 'hold':
+			case '0':
+				$status = 'hold';
+				break;
+
+			case 'approve':
+			case '1':
+				$status = 'approved';
+				break;
+
+			case 'spam':
+			case 'trash':
+			default:
+				$status = $comment_approved;
+				break;
+		}
+
+		return $status;
+	}
+
+	/**
+	 * Apply filter to comment_type and prepare it for single comment output.
+	 *
+	 * @param string  $comment_type
+	 * @return string $type
+	 */
+	protected function prepare_type_response( $comment_type ) {
+		$type = apply_filters( 'get_comment_type', $comment_type );
+
+		if ( empty( $type ) ) {
+			$type = 'comment';
+		}
+
+		return $type;
+	}
 	/**
 	 * Check if we can read a post.
 	 *
