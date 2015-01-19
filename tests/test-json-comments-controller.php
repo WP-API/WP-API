c092b6f70e2787a3a39821970d2203dd3c9f9765
Rachel Baker <rachel@rachelbaker.me>
2015-01-19 12:44:26 -0600

    Clean up the unit tests and use approved_id and hold_id properties to reuse in tests.

--- tests/test-json-comments-controller.php
+++ tests/test-json-comments-controller.php
@@ -8,17 +8,39 @@
  */
 class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase {
 
+	protected $admin_id;
+	protected $subscriber_id;
+
+	protected $post_id;
+
+	protected $approved_id;
+	protected $hold_id;
+
+	protected $endpoint;
+
 	public function setUp() {
 		parent::setUp();
 
 		$this->admin_id = $this->factory->user->create( array(
 			'role' => 'administrator',
-		) );
+		));
 		$this->subscriber_id = $this->factory->user->create( array(
 			'role' => 'subscriber',
-		) );
+		));
 
 		$this->post_id = $this->factory->post->create();
+
+		$this->approved_id = $this->factory->comment->create( array(
+			'comment_approved' => 1,
+			'comment_post_ID'  => $this->post_id,
+			'user_id'          => 0,
+		));
+		$this->hold_id = $this->factory->comment->create( array(
+			'comment_approved' => 0,
+			'comment_post_ID'  => $this->post_id,
+			'user_id'          => $this->subscriber_id,
+		));
+
 		$this->endpoint = new WP_JSON_Comments_Controller;
 	}
 
@@ -37,8 +59,6 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 
 	public function test_get_items() {
 		$this->factory->comment->create_post_comments( $this->post_id, 6 );
-		$second_post_id = $this->factory->post->create();
-		$this->factory->comment->create_post_comments( $second_post_id, 2 );
 
 		$request = new WP_JSON_Request( 'GET', '/wp/comments' );
 
@@ -46,31 +66,28 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 		$this->assertEquals( 200, $response->get_status() );
 
 		$comments = $response->get_data();
-		$this->assertCount( 8, $comments );
+		// We created 6 comments in this method, plus $this->approved_id.
+		$this->assertCount( 7, $comments );
 	}
 
 	public function test_get_items_for_post() {
-		$this->factory->comment->create_post_comments( $this->post_id, 6 );
+		$second_post_id = $this->factory->post->create();
+		$this->factory->comment->create_post_comments( $second_post_id, 2 );
 
 		$request = new WP_JSON_Request( 'GET', '/wp/comments' );
 		$request->set_query_params( array(
-			'post_id'           => $this->post_id,
+			'post_id' => $second_post_id,
 		) );
 
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 
 		$comments = $response->get_data();
-		$this->assertCount( 6, $comments );
+		$this->assertCount( 2, $comments );
 	}
 
 	public function test_get_item() {
-		$comment_id = $this->factory->comment->create( array(
-			'comment_approved' => 1,
-			'comment_post_ID'  => $this->post_id,
-		));
-
-		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $comment_id );
+		$request = new WP_JSON_Request( 'GET', sprintf( '/wp/comments/%d', $this->approved_id ) );
 
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
@@ -80,14 +97,9 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 	}
 
 	public function test_prepare_item() {
-		$comment_id = $this->factory->comment->create( array(
-			'comment_approved' => 1,
-			'comment_post_ID'  => $this->post_id,
-		));
-
-		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $comment_id );
+		$request = new WP_JSON_Request( 'GET', sprintf( '/wp/comments/%d', $this->approved_id ) );
 		$request->set_query_params( array(
-			'context'           => 'edit',
+			'context' => 'edit',
 		) );
 
 		$response = $this->server->dispatch( $request );
@@ -118,12 +130,7 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 	public function test_get_comment_not_approved() {
 		wp_set_current_user( 0 );
 
-		$comment_id = $this->factory->comment->create( array(
-			'comment_approved' => 0,
-			'comment_post_ID'  => $this->post_id,
-			'user_id'          => 0,
-		));
-		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $comment_id );
+		$request = new WP_JSON_Request( 'GET', sprintf( '/wp/comments/%d', $this->hold_id ) );
 
 		$response = $this->server->dispatch( $request );
 		$this->assertErrorResponse( 'json_user_cannot_read', $response, 401 );
@@ -132,12 +139,7 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 	public function test_get_comment_not_approved_same_user() {
 		wp_set_current_user( $this->subscriber_id );
 
-		$comment_id = $this->factory->comment->create( array(
-			'comment_approved' => 0,
-			'comment_post_ID'  => $this->post_id,
-			'user_id'          => $this->subscriber_id,
-		));
-		$request = new WP_JSON_Request( 'GET', '/wp/comments/' . $comment_id );
+		$request = new WP_JSON_Request( 'GET', sprintf( '/wp/comments/%d', $this->hold_id ) );
 
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
@@ -242,11 +244,6 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 	public function test_update_item() {
 		wp_set_current_user( $this->admin_id );
 
-		$comment_id = $this->factory->comment->create( array(
-			'comment_approved' => 1,
-			'comment_post_ID'  => $this->post_id,
-		));
-
 		$params = array(
 			'content'      => "Disco Stu doesn't advertise.",
 			'author'       => 'Disco Stu',
@@ -254,7 +251,7 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 			'author_email' => 'stu@stusdisco.com',
 			'date'         => '2014-11-07T10:14:25',
 		);
-		$request = new WP_JSON_Request( 'PUT', sprintf( '/wp/comments/%d', $comment_id ) );
+		$request = new WP_JSON_Request( 'PUT', sprintf( '/wp/comments/%d', $this->approved_id ) );
 		$request->add_header( 'content-type', 'application/json' );
 		$request->set_body( json_encode( $params ) );
 
@@ -263,7 +260,7 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 		$this->assertEquals( 201, $response->get_status() );
 
 		$comment = $response->get_data();
-		$updated = get_comment( $comment_id );
+		$updated = get_comment( $this->approved_id );
 		$this->assertEquals( $params['content'], $comment['content']['raw'] );
 		$this->assertEquals( $params['author'], $comment['author'] );
 		$this->assertEquals( $params['author_url'], $comment['author_url'] );
@@ -315,16 +312,10 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 	public function test_update_comment_invalid_permission() {
 		wp_set_current_user( 0 );
 
-		$comment_id = $this->factory->comment->create( array(
-			'comment_approved' => 0,
-			'comment_post_ID'  => $this->post_id,
-			'user_id'          => 0,
-		));
-
 		$params = array(
 			'content' => 'Disco Stu likes disco music.',
 		);
-		$request = new WP_JSON_Request( 'PUT', '/wp/comments/' . $comment_id );
+		$request = new WP_JSON_Request( 'PUT', sprintf( '/wp/comments/%d', $this->hold_id ) );
 		$request->add_header( 'content-type', 'application/json' );
 		$request->set_body( json_encode( $params ) );
 
