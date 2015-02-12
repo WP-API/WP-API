2a4f013d1478ff82e6593cddcdb61f3cd33bcbf1
Rachel Baker <rachel@rachelbaker.me>
2015-02-11 19:50:17 -0600

    Change project name to WP REST API in plugin name and Readme title.

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: JSON REST API
+ * Plugin Name: WP REST API
  * Description: JSON-based REST API for WordPress, developed as part of GSoC 2013.
  * Author: Ryan McCue, WP REST API Team
  * Author URI: http://wp-api.org
