ac3487ad5256cf2656433663ca5c9b5f6a4e7576
Daniel Bachhuber <d@danielbachhuber.com>
2015-02-28 13:02:19 -0800

    Expect nullified data when context isn't valid

--- tests/test-json-comments-controller.php
+++ tests/test-json-comments-controller.php
@@ -403,11 +403,11 @@ class WP_Test_JSON_Comments_Controller extends WP_Test_JSON_Controller_Testcase 
 		}
 
 		if ( 'edit' !== $context ) {
-			$this->assertArrayNotHasKey( 'author_ip', $data );
-			$this->assertArrayNotHasKey( 'author_user_agent', $data );
-			$this->assertArrayNotHasKey( 'date_gmt', $data );
-			$this->assertArrayNotHasKey( 'raw', $data['content'] );
-			$this->assertArrayNotHasKey( 'karma', $data );
+			$this->assertNull( $data['author_ip'] );
+			$this->assertNull( $data['author_user_agent'] );
+			$this->assertNull( $data['date_gmt'] );
+			$this->assertNull( $data['content']['raw'] );
+			$this->assertNull( $data['karma'] );
 		}
 	}
 
