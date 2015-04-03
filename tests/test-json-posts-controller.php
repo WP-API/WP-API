419524f5d74fc765451244fd0bbc47a02504bcbb
Rachel Baker <rachel@rachelbaker.me>
2015-04-03 13:20:39 -0500

    Correct taxonomy link paths in unit tests.

--- tests/test-json-posts-controller.php
+++ tests/test-json-posts-controller.php
@@ -104,13 +104,13 @@ class WP_Test_JSON_Posts_Controller extends WP_Test_JSON_Post_Type_Controller_Te
 		$attachments_url = add_query_arg( 'post_parent', $this->post_id, $attachments_url );
 		$this->assertEquals( $attachments_url, $links['attachments'][0]['href'] );
 
-		$tags_url = json_url( 'wp/terms/post_tags' );
+		$tags_url = json_url( 'wp/terms/post_tag' );
 		$tags_url = add_query_arg( 'post', $this->post_id, $tags_url );
-		$this->assertEquals( $tags_url, $links['post_tags']['href'] );
+		$this->assertEquals( $tags_url, $links['post_tag']['href'] );
 
 		$category_url = json_url( 'wp/terms/category' );
 		$category_url = add_query_arg( 'post', $this->post_id, $category_url );
-		$this->assertEquals( $category_url, $links['post_tags']['href'] );
+		$this->assertEquals( $category_url, $links['category']['href'] );
 	}
 
 	public function test_get_post_without_permission() {
