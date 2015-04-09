c8e9fe920d69f087906d69af335dcce816d4bf77
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-09 11:15:47 -0700

    Update changelog; bump version to v1.2.1

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP REST API
  * Description: JSON-based REST API for WordPress, developed as part of GSoC 2013.
- * Version: 1.2.0
+ * Version: 1.2.1
  * Author: WP REST API Team
  * Author URI: http://wp-api.org/
  * Plugin URI: https://github.com/WP-API/WP-API
@@ -13,7 +13,7 @@
  *
  * @var string
  */
-define( 'JSON_API_VERSION', '1.2.0' );
+define( 'JSON_API_VERSION', '1.2.1' );
 
 /**
  * Include our files for the API.
