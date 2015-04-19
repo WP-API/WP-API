04a38efb78c0ed716f04c6d3ac3d6b23e858fecb
Rachel Baker <rachel@rachelbaker.me>
2015-04-19 16:53:16 -0500

    Use the post_type endpoint base for the comment post link.

--- lib/endpoints/class-wp-json-comments-controller.php
+++ lib/endpoints/class-wp-json-comments-controller.php
@@ -475,10 +475,16 @@ class WP_JSON_Comments_Controller extends WP_JSON_Controller {
 		}
 
 		if ( 0 !== (int) $comment->comment_post_ID ) {
-			$links['post'] = array(
-				'href'       => json_url( '/wp/posts/' . $comment->comment_post_ID ),
-				'embeddable' => true,
-			);
+			$post = get_post( $comment->comment_post_ID );
+			if ( ! empty( $post->ID ) ) {
+				$posts_controller = new WP_JSON_Posts_Controller;
+				$base = $posts_controller->get_post_type_base( $post->post_type );
+
+				$links[ $post->post_type ] = array(
+					'href'       => json_url( '/wp/' . $base . '/' . $comment->comment_post_ID ),
+					'embeddable' => true,
+				);
+			}
 		}
 
 		if ( 0 !== (int) $comment->comment_parent ) {
