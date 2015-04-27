34f8adf80c59d7ebadd38bab74c67d8f736f5841
Ryan McCue <me@ryanmccue.info>
2015-04-27 23:04:33 +1000

    Use a shorter relation

--- lib/endpoints/class-wp-json-posts-controller.php
+++ lib/endpoints/class-wp-json-posts-controller.php
@@ -1118,7 +1118,7 @@ class WP_JSON_Posts_Controller extends WP_JSON_Controller {
 		}
 
 		if ( post_type_supports( $post->post_type, 'custom-fields' ) ) {
-			$links['http://wp-api.org/2.0/collections/meta/'] = array(
+			$links['http://wp-api.org/2.0/meta'] = array(
 				'href' => json_url( trailingslashit( $base ) . $post->ID . '/meta' ),
 				'embeddable' => true,
 			);
