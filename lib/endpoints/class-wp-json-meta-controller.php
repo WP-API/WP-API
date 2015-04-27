5f6775e1fca56fe4e8b5e75db9e0d2ced8a0f547
Ryan McCue <me@ryanmccue.info>
2015-04-27 23:33:40 +1000

    Move link into prepare method

--- lib/endpoints/class-wp-json-meta-controller.php
+++ lib/endpoints/class-wp-json-meta-controller.php
@@ -195,14 +195,7 @@ abstract class WP_JSON_Meta_Controller extends WP_JSON_Controller {
 			return new WP_Error( 'json_meta_' . $this->parent_type . '_mismatch', __( 'Meta does not belong to this object' ), array( 'status' => 400 ) );
 		}
 
-		$data = $this->prepare_item_for_response( $meta, $request );
-		if ( is_wp_error( $data ) ) {
-			return $data;
-		}
-
-		$response = new WP_JSON_Response( $data );
-		$response->add_link( 'about', json_url( 'wp/' . $this->parent_base . '/' . $parent_id ), array( 'embeddable' => true ) );
-		return $response;
+		return $this->prepare_item_for_response( $meta, $request );
 	}
 
 	/**
@@ -240,7 +233,11 @@ abstract class WP_JSON_Meta_Controller extends WP_JSON_Controller {
 			'value' => $value,
 		);
 
-		return apply_filters( 'json_prepare_meta_value', $meta, $request );
+		$parent_column = $this->get_parent_column();
+		$response = new WP_JSON_Response( $meta );
+		$response->add_link( 'about', json_url( 'wp/' . $this->parent_base . '/' . $data->$parent_column ), array( 'embeddable' => true ) );
+
+		return apply_filters( 'json_prepare_meta_value', $response, $request );
 	}
 
 	/**
