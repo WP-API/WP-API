31f3f07d08e95b1ef091115b382fc97f30f04f6b
Rachel Baker <rachel@rachelbaker.me>
2015-07-28 09:51:09 -0500

    Adjusted spacing in register_api_field docblock

--- plugin.php
+++ plugin.php
@@ -80,15 +80,30 @@ function register_rest_route( $namespace, $route, $args = array(), $override = f
 /**
  * Register a new field on an existing WordPress object type
  *
- * @global array $wp_rest_additional_fields Holds registered fields, organized by object type.
- * @param  string|array $object_type "post"|"term"|"comment" etc
- * @param  string $attribute   The attribute name
- * @param  array  $args {
- *     Optional. An array of arguments used to handle the field being registered
+ * @global array $wp_rest_additional_fields Holds registered fields, organized
+ *                                          by object type.
  *
- *     @type string|array|null $get_callback Optional. The callback function used to retrieve value of this field. If null, the default, no value will be returned in response.
- *     @type string|array|null update_callback Optional. The callback function used to update the value of this field. If null, the default, the value will not be update-able via the route this field is added to.
- *     @type string|array|null schema Optional. The callback function used to create the schema for this field. If null, the default, no schema will be returned for this field.
+ * @param  string|array $object_type Object(s) the field is being registered
+ *                                   to, "post"|"term"|"comment" etc.
+ * @param  string $attribute         The attribute name.
+ * @param  array  $args {
+ *     Optional. An array of arguments used to handle the registered field.
+ *
+ *     @type string|array|null $get_callback    Optional. The callback function
+ *                                              used to retrieve the field
+ *                                              value. Default is 'null', the
+ *                                              field will not be returned in
+ *                                              the response.
+ *     @type string|array|null $update_callback Optional. The callback function
+ *                                              used to set and update the
+ *                                              field value. Default is 'null',
+ *                                              the value cannot be set or
+ *                                              updated.
+ *     @type string|array|null schema           Optional. The callback function
+ *                                              used to create the schema for
+ *                                              this field. Default is 'null',
+ *                                              no schema entry will be
+ *                                              returned.
  * }
  * @return bool|wp_error
  */
