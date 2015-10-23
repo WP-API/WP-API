126710174c352411532e5efbb992218dbe317b15
Joe Hoyle <joehoyle@gmail.com>
2015-10-23 10:59:58 -0400

    Revert "Merge pull request #1607 from jjeaton/1588-register-post-types"
    This reverts commit 78a062a314b5637de03c9b106bfb9d2539cb864a, reversing
    changes made to 5be4a69d6aadf177b334b02a1c017de42220fb7a.

--- plugin.php
+++ plugin.php
@@ -62,7 +62,6 @@ include_once dirname( __FILE__ ) . '/lib/endpoints/class-wp-rest-posts-terms-con
 /** REST extras */
 include_once( dirname( __FILE__ ) . '/extras.php' );
 
-add_filter( 'register_post_type_args', '_add_extra_api_post_type_arguments', 10, 2 );
 add_action( 'init', '_add_extra_api_taxonomy_arguments', 11 );
 add_action( 'rest_api_init', 'create_initial_rest_routes', 0 );
 
@@ -73,30 +72,25 @@ add_action( 'rest_api_init', 'create_initial_rest_routes', 0 );
  *
  * @since 4.4.0
  *
- * @param array  $args      Array of arguments for registering a post type.
- * @param string $post_type Post type key.
+ * @global array $wp_post_types Registered post types.
  */
-function _add_extra_api_post_type_arguments( $args, $post_type ) {
-	if ( 'post' === $post_type ) {
-		$args['show_in_rest'] = true;
-		$args['rest_base'] = 'posts';
-		$args['rest_controller_class'] = 'WP_REST_Posts_Controller';
-	}
+function _add_extra_api_post_type_arguments() {
+	global $wp_post_types;
 
-	if ( 'page' === $post_type ) {
-		$args['show_in_rest'] = true;
-		$args['rest_base'] = 'pages';
-		$args['rest_controller_class'] = 'WP_REST_Posts_Controller';
-	}
+	$wp_post_types['post']->show_in_rest = true;
+	$wp_post_types['post']->rest_base = 'posts';
+	$wp_post_types['post']->rest_controller_class = 'WP_REST_Posts_Controller';
 
-	if ( 'attachment' === $post_type ) {
-		$args['show_in_rest'] = true;
-		$args['rest_base'] = 'media';
-		$args['rest_controller_class'] = 'WP_REST_Attachments_Controller';
-	}
+	$wp_post_types['page']->show_in_rest = true;
+	$wp_post_types['page']->rest_base = 'pages';
+	$wp_post_types['page']->rest_controller_class = 'WP_REST_Posts_Controller';
 
-	return $args;
+	$wp_post_types['attachment']->show_in_rest = true;
+	$wp_post_types['attachment']->rest_base = 'media';
+	$wp_post_types['attachment']->rest_controller_class = 'WP_REST_Attachments_Controller';
+
 }
+add_action( 'init', '_add_extra_api_post_type_arguments', 11 );
 
 /**
  * Adds extra taxonomy registration arguments.
