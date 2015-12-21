00a8e302a1cf7239e735a9f3e5b6575da15f9648
Rachel Baker <rachel@rachelbaker.me>
2015-12-20 21:40:56 -0600

    Use the `prepared_args` that are passed to `get_terms` to determine pagination headers

--- lib/endpoints/class-wp-rest-terms-controller.php
+++ lib/endpoints/class-wp-rest-terms-controller.php
@@ -113,8 +113,13 @@ class WP_REST_Terms_Controller extends WP_REST_Controller {
 		}
 
 		$response = rest_ensure_response( $response );
+
+		// Store pagation values for headers then unset for count query.
+		$per_page = (int) $prepared_args['number'];
+		$page = ceil( ( ( (int) $prepared_args['offset'] ) / $per_page ) + 1 );
 		unset( $prepared_args['number'] );
 		unset( $prepared_args['offset'] );
+
 		$total_terms = wp_count_terms( $this->taxonomy, $prepared_args );
 
 		// wp_count_terms can return a falsy value when the term has no children
@@ -123,20 +128,20 @@ class WP_REST_Terms_Controller extends WP_REST_Controller {
 		}
 
 		$response->header( 'X-WP-Total', (int) $total_terms );
-		$max_pages = ceil( $total_terms / $request['per_page'] );
+		$max_pages = ceil( $total_terms / $per_page );
 		$response->header( 'X-WP-TotalPages', (int) $max_pages );
 
 		$base = add_query_arg( $request->get_query_params(), rest_url( '/wp/v2/' . $this->get_taxonomy_base( $this->taxonomy ) ) );
-		if ( $request['page'] > 1 ) {
-			$prev_page = $request['page'] - 1;
+		if ( $page > 1 ) {
+			$prev_page = $page - 1;
 			if ( $prev_page > $max_pages ) {
 				$prev_page = $max_pages;
 			}
 			$prev_link = add_query_arg( 'page', $prev_page, $base );
 			$response->link_header( 'prev', $prev_link );
 		}
-		if ( $max_pages > $request['page'] ) {
-			$next_page = $request['page'] + 1;
+		if ( $max_pages > $page ) {
+			$next_page = $page + 1;
 			$next_link = add_query_arg( 'page', $next_page, $base );
 			$response->link_header( 'next', $next_link );
 		}
