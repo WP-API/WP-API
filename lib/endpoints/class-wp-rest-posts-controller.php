be28d27242492efdb5e16818daff9d2500012c55
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-11 05:53:57 -0800

    Ensure media of private posts are private too

--- lib/endpoints/class-wp-rest-posts-controller.php
+++ lib/endpoints/class-wp-rest-posts-controller.php
@@ -943,10 +943,7 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 		// Can we read the parent if we're inheriting?
 		if ( 'inherit' === $post->post_status && $post->post_parent > 0 ) {
 			$parent = get_post( $post->post_parent );
-
-			if ( $this->check_read_permission( $parent ) ) {
-				return true;
-			}
+			return $this->check_read_permission( $parent );
 		}
 
 		// If we don't have a parent, but the status is set to inherit, assume
