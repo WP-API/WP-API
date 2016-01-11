bcffb19edabd4358d86c569e0e8e317db4489614
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-11 08:54:48 -0800

    Bump version to 1.2.5

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP REST API
  * Description: JSON-based REST API for WordPress, developed as part of GSoC 2013.
- * Version: 1.2.4
+ * Version: 1.2.5
  * Author: WP REST API Team
  * Author URI: http://wp-api.org/
  * Plugin URI: https://github.com/WP-API/WP-API
@@ -13,7 +13,7 @@
  *
  * @var string
  */
-define( 'JSON_API_VERSION', '1.2.4' );
+define( 'JSON_API_VERSION', '1.2.5' );
 
 /**
  * Include our files for the API.
