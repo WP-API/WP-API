e9b58186cf88893d410b2fa97413dc1ef68c2aad
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-14 12:18:27 -0800

    Fix PHPCS issue

--- lib/endpoints/class-wp-rest-attachments-controller.php
+++ lib/endpoints/class-wp-rest-attachments-controller.php
@@ -11,7 +11,7 @@ class WP_REST_Attachments_Controller extends WP_REST_Posts_Controller {
 	 */
 	protected function prepare_items_query( $prepared_args = array() ) {
 		$query_args = parent::prepare_items_query( $prepared_args );
-		if ( empty( $query_args['post_status'] ) || ! in_array( $query_args['post_status'], array( 'inherit', 'private', 'trash' ) ) ){
+		if ( empty( $query_args['post_status'] ) || ! in_array( $query_args['post_status'], array( 'inherit', 'private', 'trash' ) ) ) {
 			$query_args['post_status'] = 'inherit';
 		}
 		return $query_args;
