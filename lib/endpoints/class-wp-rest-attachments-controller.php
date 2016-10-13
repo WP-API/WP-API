549172ba290bc3ec0b25f949f5b69fd295fa7a1c
miya0001 <miya@wpist.me>
2016-10-13 11:52:41 +0900

    remove ununsed variable $post_type_obj

--- lib/endpoints/class-wp-rest-attachments-controller.php
+++ lib/endpoints/class-wp-rest-attachments-controller.php
@@ -40,7 +40,6 @@ class WP_REST_Attachments_Controller extends WP_REST_Posts_Controller {
 			return $ret;
 		}
 
-		$post_type_obj = get_post_type_object( $this->post_type );
 		if ( ! current_user_can( 'upload_files' ) ) {
 			return new WP_Error( 'rest_cannot_create', __( 'Sorry, you are not allowed to upload media on this site.' ), array( 'status' => 400 ) );
 		}
