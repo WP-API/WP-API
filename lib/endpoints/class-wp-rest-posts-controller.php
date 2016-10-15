d9ecf34ba96404f18f70a9069306f5f7852e8c07
Adam Silverstein <adam@10up.com>
2016-10-15 13:47:35 -0400

    remove extraneous object check

--- lib/endpoints/class-wp-rest-posts-controller.php
+++ lib/endpoints/class-wp-rest-posts-controller.php
@@ -797,7 +797,7 @@ class WP_REST_Posts_Controller extends WP_REST_Controller {
 			}
 		}
 		// Post slug.
-		if ( ! empty( $schema['properties']['slug'] ) && isset( $request['slug'] ) && ! empty( $request['slug'] ) && ! is_object( $request['slug'] ) ) {
+		if ( ! empty( $schema['properties']['slug'] ) && isset( $request['slug'] ) && ! empty( $request['slug'] ) ) {
 			$prepared_post->post_name = $request['slug'];
 		}
 
